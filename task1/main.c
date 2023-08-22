70e2b09baf2da4fd31b366b984508f24985eefc1
AndreyNSTU1 <114638456+AndreyNSTU1@users.noreply.github.com>
2023-08-22 16:02:33 +0700

    Update main.c

--- task1/main.c
+++ task1/main.c
@@ -12,7 +12,7 @@ int result1;
 
 result = multiply(a, b);
 printf("Multiplication result: %d\n", result);
-exit(EXIT_SUCCESS);
+
 
 result1 = division(a, b);
 printf("Division result: %d\n", result1);
